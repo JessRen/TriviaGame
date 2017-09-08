94be54ff55c767eb358391843a61e2e7eda0c9de
Jessica <jessicalrenard@gmail.com>
2017-09-08 19:23:37 -0400

    Updates from office hours

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,66 +1,67 @@
-console.log("working");
 
-// Combining slideshow with Bootstrap and Timers with Conditionals
+
+  $(document).ready( function() {
+    // Combining slideshow with Bootstrap and Timers with Conditionals
 
 
 // TODO: Put links to our images in this image array.
 var qimages = [
   {
-  qimage: "../assets/images/bougainvilleas.jpg", 
+  qimage: "assets/images/bougainvilleas.jpg", 
   choices: ["bougainvilleas", "hydrangea", "rodedendron"],
   correctAnswer: 0
   },
 
   {
-  qimage: "../assets/images/cherry-blossoms.jpg", 
+  qimage: "assets/images/cherry-blossoms.jpg", 
   choices: ["bougainvilleas", "cherry-blossoms", "rodedendron"],
   correctAnswer: 1
   },
 
   {
-  qimage: "../assets/images/dahlia.jpg", 
+  qimage: "assetsimages/dahlia.jpg", 
   choices: ["dahlia", "peony", "daisy"],
   correctAnswer: 0
   },
 
   {
-  qimage: "../assets/images/delphinium-larkspur.jpg", 
+  qimage: "assets/images/delphinium-larkspur.jpg", 
   choices: ["hydrangea", "delphinium", "larkspark"],
   correctAnswer: 1
   },
 
   {
-  qimage: "../assets/images/gladiolus.jpg", 
+  qimage: "assets/images/gladiolus.jpg", 
   choices: ["tiger lily", "hydrangea", "gladiolus"],
   correctAnswer: 2
   },
 
   {
-  qimage: "../assets/images/honeysuckle.jpg", 
+  qimage: "assets/images/honeysuckle.jpg", 
   choices: ["tulip", "lily", "honeysuckle"],  
   correctAnswer: 2
   },
 
   {
-  qimage: "../assets/images/hydrangea.jpg", 
+  qimage: "assets/images/hydrangea.jpg", 
   choices: ["iris", "hydrangea", "bougainvilleas"],
   correctAnswer: 1
   },
 
   {
-  qimage: "../assets/images/iris.jpg", 
+  qimage: "assets/images/iris.jpg", 
   choices: ["tiger lily", "hydrangea", "iris"], 
   correctAnswer: 2
   },
 
   {
-  qimage: "../assets/images/peony.jpg", 
+  qimage: "assets/images/peony.jpg", 
   choices: ["gerbera daisy", "peony", "cornflower"], 
   correctAnswer: 1
   },
 
   {
-  qimage: "../assets/images/rose.jpg",
+  qimage: "assets/images/rose.jpg",
   choices: ["carnation", "hydrangea", "rose"], 
   correctAnswer: 2
   }];
@@ -69,73 +70,50 @@ var qimages = [
 // Variable showImage will hold the setInterval when we start the slideshow
 var showQimage;
 
-// Count will keep track of the index of the currently displaying query-picture.
-var count = 0;
+// Qcount will keep track of the index of the currently displaying query-picture.
+var qcount = 0;
 
 //*** Advance Feature: Later - Introduce Answers *** need logic below for countCorrect and countWrong
 
 //var answerImages = [ , ,]
 
 //Variable showAnswer can hold setTimeout for answer pauses
-var showAnswer;
+//var showAnswer;
 ///*** need logic below for countCorrect and countWrong 
 //*** Track answers as correct or wrong ****
 var countCorrect = 0;
 
 var countWrong = 0;
 
-// showImage integrates image files for the #image-holder div 
-// in the 'src' attribute of the img tag.
-function showQimage() {
-  $("#image-holder").html("<img src=" + qimages[count].qimage + " width='300px'>");
-}
-//next image keeps the slideshow going down the line.
-function nextQimage() {
 
-  // Increments the count (index of the current feature photo) by 1.
-  count++;
-  if(count === qimages.length) {
-    count = 0;
-  }
-}
-// tie together showImage with nextImage and with automatic setInterval between them to 10 seconds.
-  function startSlideshow() {
-    showQimage  = setInterval(nextQimage, 10000);
-  }
-//start slideshow automatically
-  $(document).ready( function() {
     
-//  Variable that will hold our setInterval that runs the stopwatch
+//  Variable that will hold our setInterval that runs the counter
 var intervalId;
 
 //prevents the clock from being sped up unnecessarily
 var clockRunning = false;
 
-//  Our stopwatch object.
-var stopwatch = {
+//  Our counter object.
+var counter = {
 
-  time: 60,
+  time: 15,
   
   reset: function() {
 
-    stopwatch.time = 0;
-    stopwatch.lap = 1;
+    counter.time = 0;
     $("#time-left").html("00:00");
-    $("#laps").empty();
-
     clockRunning = false;
 
-    //  TODO: Change the "display" div to "00:00."
+    // Change the "display" div to "00:00."
 
   },
 
   start: function() {
 
-      //  TODO: Use setInterval to start the count here and set the clock to running.
+      //  Use setInterval to start the count here and set the clock to running.
       if (!clockRunning) {
         // CLEARS THE BINDING - USING INCREMENT WOULD THROW THE BINDING
-        intervalId = setInterval(stopwatch.count, 1000); 
-
+        intervalId = setInterval(counter.count, 1000); 
         clockrunning = true;
 
       }
@@ -155,11 +133,12 @@ var stopwatch = {
   count: function() {
 
     //  TODO: increment time by 1, remember we cant use "this" here.
-      stopwatch.time++;
-    //  TODO: Get the current time, pass that into the stopwatch.timeConverter function,
+      counter.time--;
+    //  TODO: Get the current time, pass that into the counter.timeConverter function,
     //        and save the result in a variable.
-      var currentTime = stopwatch.timeConverter(stopwatch.time);
+      var currentTime = counter.timeConverter(counter.time);
     //  TODO: Use the variable you just created to show the converted time in the "display" div.
+    console.log(currentTime)
     $("#time-left").html(currentTime);
   },
 
@@ -173,54 +152,64 @@ var stopwatch = {
     var seconds = t - (minutes * 60);
 
     if (seconds < 10) {
-      seconds = "0" + seconds;
+      seconds = "" + seconds;
     }
 
-    if (minutes === 0) {
-      minutes = "00";
+    if (minutes < 10) {
+      minutes = "" + minutes;
     }
 
-    else if (minutes < 10) {
-      minutes = "0" + minutes;
-    }
-
-    return minutes + ":" + seconds;
+    return minutes + "" + seconds;
   }
 };
+  // showImage integrates image files for the #image-holder div 
+// in the 'src' attribute of the img tag.
+function showQimage() {
+  $("#image-holder").html("<img src=" + qimages[qcount].qimage + " width='300px'>");
+}
+//next image keeps the slideshow going down the line.
+function nextQimage() {
+
+  // Increments the qcount (index of the current feature photo) by 1.
+  qcount++;
+  if(qcount === qimages.length) {
+    qcount = 0;
+  }
+}
+// tie together showImage with nextImage and with automatic setInterval between them to 10 seconds.
+  function startSlideshow() {
+    console.log("working");
+    counter.start();
+    // showQimage  = setInterval(nextQimage, 10000);
+    showQimage();
 
 
+  }
+//start slideshow automatically
+     
+         //Use jQuery to run "startSlideshow" when we click the "restart" button at conclusion of quiz
+      $("#start-butt").click(startSlideshow);
 
-     stopwatch.start();
-     startSlideshow();
+     
+    function stopSlideshow() {
+
+      //retain clearInterval !!
+      clearInterval(showQimage);
+    };
+
   });
 
 //GRADE THE ANSWERS SOMEWHERE WITH IF/THEN CONDITONS
 // **** radio buttons must populate varied options with one being correct answer, and upon selection and/or timeout, program must count answer as correct or not, and announce so, and provide correct answer if incorrect answer given.-->
 
 // Answer array for sequence of answers.
-/*function showAnswers() {
-  $("#image-holder").html("<img src=" + answerImages[count] + "width='300px'>");
-}
+//function showAnswers() {
+ // $("#image-holder").html("<img src=" + answerImages[count] + "width='300px'>");
+//}
 
-  // Tailor setTimeout to pause after the timer runs out and between the answer image and the next question image, for 3 seconds. 
-setTimeout(showAnswers, 3000);*/
 
-  //If the count is the same as the length of the image array, reset the count to 0.
-if(count === answerImages.length) {
-  count = 0;
-   stopSlideshow();
-  }
-};
+  
 
-//Use jQuery to run "startSlideshow" when we click the "restart" button at conclusion of quiz
-  $(".btn").click(startSlideshow);
-
- 
-function stopSlideshow() {
-
-  //retain clearInterval !!
-  clearInterval(showQimage);
-};
 
 
 //include inline radio button answers 
